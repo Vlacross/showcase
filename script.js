9745eaf4ee3e52d0b9dab17ae4c72a49cabf743f
Tim Vlasuk <timvlasuk@gmail.com>
2019-03-20 13:49:36 -0700

    brainstorming simple mock layout

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+function loadContent() {
+
+};
+
+
+
+
+
+
+
+
+$(document).ready(() => {
+  loadContent()
+})
+
---
+++ templates.js
@@ -0,0 +1,11 @@
+const desktopHeader = 
+`
+<div  class="deskHeader">
+  <div class="deskHeaderLeft"></div>
+  <div class="deskHeaderCenter">
+  
+  </div>
+  <div class="deskHeaderLeftRight"></div>
+
+</div>
+`
