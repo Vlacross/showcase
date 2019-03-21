c90b8b7ac3d88b3dfdff2ae1beec51c0fe372033
Tim Vlasuk <timvlasuk@gmail.com>
2019-03-20 19:55:13 -0700

    playing around with expensive css. building header

--- script.js
+++ script.js
@@ -1,19 +1,144 @@
 
 
-
-
-function loadContent() {
+function fancyShow() {
+  $('.deskHeaderLeft').addClass('shadowBottom')
+  setTimeout(() => {
+    $('.deskHeaderLeft').removeClass('shadowBottom')
+    $('.deskHeaderLeft').addClass('shadowRight')
+  }, 75)
+  setTimeout(() => {
+    $('.deskHeaderLeft').removeClass('shadowRight')
+    $('.deskHeaderCenter').addClass('shadowLeft')
+  }, 150)
+  setTimeout(() => {
+    $('.deskHeaderCenter').removeClass('shadowLeft')
+    $('.deskHeaderCenter').addClass('shadowTop')
+  }, 225)
+  setTimeout(() => {
+    $('.deskHeaderCenter').removeClass('shadowTop')
+    $('.deskHeaderCenter').addClass('shadowRight')
+  }, 300)
+  setTimeout(() => {
+    $('.deskHeaderCenter').removeClass('shadowRight')
+    $('.deskHeaderRight').addClass('shadowLeft')
+  }, 375)
+  setTimeout(() => {
+    $('.deskHeaderRight').removeClass('shadowLeft')
+    $('.deskHeaderRight').addClass('shadowBottom')
+  }, 450)
+  setTimeout(() => {
+    $('.deskHeaderRight').removeClass('shadowBottom')
+    $('.deskHeaderRight').addClass('shadowRight')
+  }, 525)
+  setTimeout(() => {
+    $('.deskHeaderRight').removeClass('shadowRight')
+    $('.deskHeaderRight').addClass('shadowTop')
+  }, 600)
+  setTimeout(() => {
+    $('.deskHeaderRight').removeClass('shadowTop')
+    $('.deskHeaderRight').addClass('shadowLeft')
+  }, 675)
+  setTimeout(() => {
+    $('.deskHeaderRight').removeClass('shadowLeft')
+    $('.deskHeaderCenter').addClass('shadowRight')
+  }, 750)
+  setTimeout(() => {
+    $('.deskHeaderCenter').removeClass('shadowRight')
+    $('.deskHeaderCenter').addClass('shadowBottom')
+  }, 825)
+  setTimeout(() => {
+    $('.deskHeaderCenter').removeClass('shadowBottom')
+    $('.deskHeaderCenter').addClass('shadowLeft')
+  }, 900)
+  setTimeout(() => {
+    $('.deskHeaderCenter').removeClass('shadowLeft')
+    $('.deskHeaderLeft').addClass('shadowRight')
+  }, 975)
+  setTimeout(() => {
+    $('.deskHeaderLeft').removeClass('shadowRight')
+    $('.deskHeaderLeft').addClass('shadowTop')
+  }, 1050)
+  setTimeout(() => {
+    $('.deskHeaderLeft').removeClass('shadowTop')
+    $('.deskHeaderLeft').addClass('shadowLeft')
+  }, 1125)
 
 };
 
+function introTitle() {
+  setTimeout(() => {
+    $('.b1').addClass('shadowRight')
+  }, 75)
+  setTimeout(() => {
+    $('.b1').replaceWith(introLetter1)
+    // $('.introLetter 1').addClass('shadowTop')
+  }, 150)
+  setTimeout(() => {
+    $('.b2').addClass('shadowRight')
+  }, 225)
+  setTimeout(() => {
+    $('.b2').replaceWith(introLetter2)
+    // $('.introLetter 2').addClass('shadowTop')
+  }, 300)
+  setTimeout(() => {
+    $('.b3').addClass('shadowRight')
+  }, 375)
+  setTimeout(() => {
+    $('.b3').replaceWith(introLetter3)
+    // $('.introLetter 3').addClass('shadowTop')
+  }, 450)
+  setTimeout(() => {
+    $('.b4').addClass('shadowRight')
+  }, 525)
+  setTimeout(() => {
+    $('.b4').replaceWith(introLetter4)
+    // $('.introLetter 4').addClass('shadowTop')
+  }, 600)
+}
+
+// $('.deskHeaderLeft')
+
+// $('.deskHeaderCenter')
+
+// $('.deskHeaderRight')
+
+// shadowLeft
+
+// shadowRight
+
+// shadowTop
+
+// shadowBottom 
 
 
 
 
+function loadContent() {
+  $('.wrapper').replaceWith(introHeader)
+  fancyShow()
+  introTitle()
+};
 
 
 
 $(document).ready(() => {
   loadContent()
-})
+});
 
+
+
+
+/*
+$(document).ready(function(){
+  predictLocation();
+
+  showText($('.1'));
+  setTimeout(function() {
+    showText($('.2'));}, 1000);
+  setTimeout(function() {
+    showText($('.3'));}, 2000);
+  setTimeout(function() {
+    showText($('.4'));}, 3000); 
+
+});
+ */
--- templates.js
+++ templates.js
@@ -1,11 +1,51 @@
-const desktopHeader = 
+const introHeader = 
 `
-<div  class="deskHeader">
+<div  class="wrapper">
   <div class="deskHeaderLeft"></div>
   <div class="deskHeaderCenter">
-  
+  <div class="box b1"></div>
+  <div class="box b2"></div>
+  <div class="box b3"></div>
+  <div class="box b4"></div>
   </div>
-  <div class="deskHeaderLeftRight"></div>
+  <div class="deskHeaderRight"></div>
 
 </div>
 `
+
+const introLetter1 = 
+`
+<span class="introLetter 1">T</span>
+`;
+
+const introLetter2 = 
+`
+<span class="introLetter 2">i</span>
+`;
+
+const introLetter3 = 
+`
+<span class="introLetter 3">m</span>
+`;
+
+const introLetter4 = 
+`
+<span class="introLetter 4">V</span>
+`;
+
+const profileHeader = 
+`
+<div class="profileHeader">
+
+  <h1 class="name"></h1>
+  <span class="name">T</span>
+  <span class="name">i</span>
+  <span class="name">m</span>
+  <span class="name">V</span>
+
+  <nav>
+  
+  </nav>
+</div>
+`
+
